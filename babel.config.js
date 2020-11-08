51cf4a375d8ad243d02e574cd1fa93f7ed3c79f4
MisterTicot <mister.ticot@cosmic.plus>
2020-11-08 22:20:01 +0100

    meta Change: improve browser support

--- babel.config.js
+++ babel.config.js
@@ -2,11 +2,13 @@ module.exports = function (api) {
   api.cache(true)
 
   return {
-    presets: ["@babel/preset-env"],
-    plugins: [
-      "@babel/plugin-transform-runtime",
-      "@babel/plugin-syntax-dynamic-import"
+    presets: [
+      [
+        "@babel/preset-env",
+        { useBuiltIns: "usage", corejs: 3, exclude: ["es.promise"] }
+      ]
     ],
+    plugins: ["@babel/plugin-transform-runtime"],
     sourceType: "unambiguous"
   }
 }
